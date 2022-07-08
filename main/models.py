dd6ea66486bfc718b1f4466da98561d869489926
Aaris-Kazi <aariskazi@gmail.com>
2022-07-08 23:44:59 +0530

    database changes books added

--- main/models.py
+++ main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Books(models.Model):
+    book_name = models.CharField(max_length=50)    
+    status = models.CharField(max_length=50,default='Available')   
+    def __str__(self):
+        return self.book_name+""+str(self.status)
